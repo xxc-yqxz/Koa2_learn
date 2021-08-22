73a2147fd5c2a7c29419e2069294c4be69b04869
xxc7 <894043590@qq.com>
2021-08-22 11:21:48 +0800

    1

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const Koa = require('koa');
+const path = require('path')
+const koaNunjucks = require('koa-nunjucks-2')
 
 const router = require('./app/router');
 
 const app = new Koa();
 
+app.use(koaNunjucks({
+    ext: 'html', // 使用HTML后缀的模板
+    path: path.join(__dirname, 'app', 'view'),   // 模板所在路径
+    nunjucksConfig: {   // nunjucks的配置
+        trimBlocks: true    // 去除标签后的换行
+    }
+}))
+
 app.use(async (ctx, next) => {
     await next();
     if (ctx.status === 404) {
-        ctx.body = '404';
+        await ctx.render('404', {})
     }
 })
 
