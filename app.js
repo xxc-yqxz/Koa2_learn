2afdadde31b913452efb766e5347ab1025933371
xxc7 <894043590@qq.com>
2021-08-22 11:41:58 +0800

    1

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const Koa = require('koa');
 const path = require('path')
 const koaNunjucks = require('koa-nunjucks-2')
 
+const log4js = require('log4js')
+log4js.configure({
+    // 指定输出文件类型和文件名
+    appenders: { cheese: { type: "file", filename: "cheese.log" }, xxc: { type: "file", filename: "xxc.log" } },
+    // appenders 指定了日志追加到 cheese
+    // level 设置为 error
+    categories: { default: { appenders: ["cheese"], level: "error" }, xxc: { appenders: ["xxc"], level: "debug" } }
+})
+const logger = log4js.getLogger();  // 此处getLogger()不传参数，则代表使用categories中的default
+// logger.level = 'error'; // 此时设置日志等级后，之后下一行的代码只能输出比设置等级高的日志
+logger.error('输出日志');
+const xxcLogger = log4js.getLogger('xxc')
+xxcLogger.debug('hahaha')
+
 const router = require('./app/router');
 
 const app = new Koa();
